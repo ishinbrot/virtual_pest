231d54d69b23424a7471b50df7736a1a27d87f9d
Ian Shinbrot <ianshinbro@gmail.com>
2020-03-30 15:23:13 -0400

    Updates

---
+++ precache-manifest.1424f26bcec95f8023c6c226f856693b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979e5974d813d866aa8b4365ac5a7a7a",
+    "url": "/virtual_pest/index.html"
+  },
+  {
+    "revision": "defb591cd9af23ce1cda",
+    "url": "/virtual_pest/static/css/main.d0ab0c92.chunk.css"
+  },
+  {
+    "revision": "e3669afd72511832b0af",
+    "url": "/virtual_pest/static/js/2.fda2cec1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/virtual_pest/static/js/2.fda2cec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "defb591cd9af23ce1cda",
+    "url": "/virtual_pest/static/js/main.5bcd1348.chunk.js"
+  },
+  {
+    "revision": "ff240637a2f12c480c37",
+    "url": "/virtual_pest/static/js/runtime-main.94095413.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/virtual_pest/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3630001d1a0344f58bc9ed759f11f3eb",
+    "url": "/virtual_pest/static/media/penguin_dead.3630001d.png"
+  },
+  {
+    "revision": "265db23c14f717053f59e88059bb2357",
+    "url": "/virtual_pest/static/media/penguin_enraged.265db23c.png"
+  },
+  {
+    "revision": "2d5a4e043eaeaabeb08bb45297ca8d8b",
+    "url": "/virtual_pest/static/media/penguin_happy.2d5a4e04.png"
+  },
+  {
+    "revision": "53d29cc1bac39049ef3f4a18a8f0f1ba",
+    "url": "/virtual_pest/static/media/penguin_hungry.53d29cc1.png"
+  },
+  {
+    "revision": "f6c2fee914c2788f5e0fb49fce612c42",
+    "url": "/virtual_pest/static/media/penguin_playful.f6c2fee9.png"
+  }
+]);
